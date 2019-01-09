c610118e26803511d7e85da3e05d4062a8f92b4a
Pavel Zika <pzika@langmaster.cz>
2019-01-09 22:23:31 +0100

    word break libs

---
+++ clibs/Fulltext/FtxLibLow.cs
@@ -0,0 +1,61 @@
+﻿/*
+ - Problémy: délka phrase max. 255 znaků (včetně závorek)
+ - závorky uprostřed slova se pro fulltext odstraní (vy)konat = konat
+ */
+using LangsLib;
+using Microsoft.EntityFrameworkCore;
+using SpellChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Fulltext {
+
+
+	public static class FtxLibLow {
+		//Doplni text.Idxs pro text, zbavený závorek
+		public static void STAWordBreak(Langs lang, PhraseWords text) {
+			text.Idxs = StemmerBreaker.RunBreaker.STAWordBreak(lang, noBrackets(text.Text));
+		}
+
+		//Pro SpellCheck error nastavi zaporne Len.
+		public static void STASpellCheck(Langs lang, SelectedWords selected) {
+			if (selected.selected == null || selected.selected.Length == 0) return;
+			//Spell check
+			var errorIdxs = RunSpellCheckWords.STACheck(lang, selected.selected);
+			//update Len for wrong words
+			if (errorIdxs != null) foreach (var errIdx in errorIdxs) selected.phrase.Idxs[errIdx] = new TPosLen() { Pos = selected.phrase.Idxs[errIdx].Pos, Len = (sbyte)-selected.phrase.Idxs[errIdx].Len };
+		}
+
+		public static string[] StemmingWithSQLServer(Langs lang, string phrase) {
+			var ctx = new FulltextContext();
+			var sql = string.Format("SELECT display_term FROM sys.dm_fts_parser('FormsOf(INFLECTIONAL, \"{0}\")', {1}, 0, 1)", phrase.Replace("'", "''")/*https://stackoverflow.com/questions/5528972/how-do-i-convert-a-string-into-safe-sql-string*/, Metas.lang2LCID(lang));
+			return ctx.Set<dm_fts_parser>().FromSql(sql).Select(p => p.display_term).ToArray();
+		}
+
+		public static PhraseWords STABreakAndCheck(Langs lang, string phrase) {
+			if (string.IsNullOrEmpty(phrase)) return null;
+			var newText = new PhraseWords(phrase);
+			//WordBreaking text (text in brackets excluded)
+			STAWordBreak(lang, newText);
+			//Spell check
+			STASpellCheck(lang, new SelectedWords(newText)); //low level spell check
+			return newText;
+		}
+
+		//Bracket parsing
+		public static IEnumerable<Bracket> BracketParse(string s) {
+			foreach (Match match in otherBrackets.Matches(s)) yield return new Bracket { Br = match.Value[0], Text = match.Value.Substring(1, match.Value.Length - 2) };
+			foreach (Match match in roundBrackets.Matches(s)) yield return new Bracket { Br = match.Value[0], Text = match.Value.Substring(1, match.Value.Length - 2) };
+		}
+		static string noBrackets(string text) { return roundBrackets.Replace(otherBrackets.Replace(text, match => new String(' ', match.Length)), match => new String(' ', match.Length)); }
+
+		public struct Bracket { public char Br; public string Text; }
+		static Regex roundBrackets = new Regex(@"\((.*?)\)");
+		static Regex otherBrackets = new Regex(@"\{(.*?)\}|\[(.*?)\]");
+
+	}
+
+}
+
---
+++ clibs/Fulltext/Model.cs
@@ -0,0 +1,118 @@
+﻿//https://docs.microsoft.com/en-us/ef/core/miscellaneous/cli/powershell
+//Add-Migration InitialCreate or v1 or v10 ...
+//drop-database
+//update-database 
+using LangsLib;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace Fulltext {
+
+  public class PhraseWord {
+
+		public const string PhraseIdName = "PhraseId";
+		public const string SrcLangName = "SrcLang";
+		public const string DestLangName = "DestLang";
+
+		[Key]
+		public int Id { get; set; } //internal unique ID
+		[MaxLength(PhraseWords.maxWordLen), Required]
+		public string Word { get; set; }
+		public int PhraseRef { get; set; } //ID of phrase, containing word. Could be Hash64 of string, identifying phrase in its source repository.
+		public byte SrcLang { get; set; }
+		public byte DestLang { get; set; }
+
+		public Phrase Phrase { get; set; }
+	}
+
+	public class Phrase {
+
+		public const int maxPhraseBaseLen = 128;
+
+		[Key]
+		public int Id { get; set; } //internal unique ID
+		[Required]
+		public string Text { get; set; }
+		[Required]
+		public byte[] TextIdxs { get; set; } //word breking and stemming result (<pos, len> array)
+		public byte SrcLang { get; set; }
+		public byte DestLang { get; set; }
+		public int? SrcRef { get; set; }
+    public int DictRef { get; set; }
+
+    [MaxLength(maxPhraseBaseLen), Required]
+		public string Base { get; set; } //normalized text for duplicity search
+
+		public ICollection<PhraseWord> Words { get; set; } //stemmed words
+		public ICollection<Phrase> Dests { get; set; }
+		public Phrase Src { get; set; }
+		public Dict Dict { get; set; }
+	}
+
+	public class Dict {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public byte SrcLang { get; set; }
+		public DateTime Imported { get; set; }
+		public ICollection<Phrase> Phrases { get; set; } 
+	}
+
+
+
+	//fake entity for dm_fts_parser result, see //https://github.com/aspnet/EntityFramework/issues/245 register entity
+	public class dm_fts_parser {
+		[Key]
+		public string display_term { get; set; }
+	}
+
+	public class FulltextContext : DbContext {
+		public DbSet<PhraseWord> PhraseWords { get; set; }
+		public DbSet<Phrase> Phrases { get; set; }
+		public DbSet<Dict> Dicts { get; set; }
+		//https://github.com/aspnet/EntityFramework/issues/245 register fake dm_fts_parser entity
+		public DbSet<dm_fts_parser> dm_fts_parser { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["FulltextDatabase"].ConnectionString);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {
+			//http://www.learnentityframeworkcore.com/configuration/one-to-many-relationship-configuration
+			//modelBuilder.Entity<PhraseWord>().HasIndex(p => p.Word);
+			modelBuilder.Entity<PhraseWord>().HasIndex(p => new { p.Word, p.SrcLang, p.DestLang });
+
+			modelBuilder.Entity<Phrase>()
+				.HasMany(c => c.Words)
+				.WithOne(e => e.Phrase)
+				.IsRequired()
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder.Entity<Phrase>()
+				.HasMany(c => c.Dests)
+				.WithOne(e => e.Src)
+				.HasForeignKey(b => b.SrcRef)
+				.OnDelete(DeleteBehavior.Restrict); //https://stackoverflow.com/questions/22681352/entity-framework-6-code-first-cascade-delete-on-self-referencing-entity
+			modelBuilder.Entity<Phrase>().HasIndex(p => new { p.Base, p.SrcLang, p.DestLang });
+
+			modelBuilder.Entity<Dict>().HasIndex(p => p.Name);
+			modelBuilder.Entity<Dict>()
+				.HasMany(c => c.Phrases)
+				.WithOne(e => e.Dict)
+        .HasForeignKey(e => e.DictRef)
+        .IsRequired()
+				.OnDelete(DeleteBehavior.Cascade);
+		}
+
+		public void recreate() {
+			Database.EnsureDeleted();
+			Database.EnsureCreated();
+		}
+
+
+	}
+
+}
