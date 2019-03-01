457da5bf7e1397ff80cdba37aa70e91c52b96759
Pavel Zika <pzika@langmaster.cz>
2019-03-01 22:29:12 +0100

    fix

--- dlibs/low-utils/lib/src/packer/readers/bitReader.dart
+++ dlibs/low-utils/lib/src/packer/readers/bitReader.dart
@@ -2,9 +2,9 @@ import 'dart:typed_data';
 import 'byteReader.dart';
 
 class BitReader {
-  var _readData = 0;
-  var _startPosition = 0;
-  var _readDataEmpty = true;
+  var _buf = 0;
+  var _bufPos = 0;
+  var _bufEmpty = true;
 
   ByteReader dataStream;
 
@@ -12,7 +12,7 @@ class BitReader {
     dataStream = new ByteReader(data);
   }
 
-  int get bitsInBuffer => _readDataEmpty ? 0 : 8 - _startPosition;
+  int get bitsToRead => _bufEmpty ? 0 : 8 - _bufPos;
 
   bool readBit() {
     return readBits(1).first;
@@ -21,28 +21,42 @@ class BitReader {
   Iterable<bool> readAllBits() sync* {
     while (true) {
       // return the rest of the readData
-      for (var b in readBits(bitsInBuffer)) yield b;
+      for (var b in readBits(bitsToRead)) yield b;
       final bb = dataStream.tryReadByte();
       if (bb == null) break;
-      for (var i = 0; i < 8; i++) yield (bb & (1 << (31 - i - 1))) != 0;
+      for (var i = 0; i < 8; i++) yield isBit(bb, i);
     }
   }
 
   Iterable<bool> readBits(int bitCount) sync* {
     while (bitCount > 0) {
       bitCount--;
-      if (bitsInBuffer == 0) {
-        _readData = dataStream.readByte() << 24;
-        _startPosition = 0;
-        _readDataEmpty = false;
+      if (bitsToRead == 0) {
+        _buf = dataStream.readByte();
+        _bufPos = 0;
+        _bufEmpty = false;
       }
-      yield (_readData & (1 << (31 - _startPosition))) != 0;
-      _startPosition++;
+      yield isBit(_buf, _bufPos);
+      _bufPos++;
     }
   }
 
-  void Align() {
-    _startPosition = 0;
-    _readData = 0;
+  void align() {
+    _bufPos = 0;
+    _buf = 0;
+    _bufEmpty = true;
   }
 }
+
+bool isBit(int byte, int idx /*0..7*/) => (byte & idxBitsMask[idx]) == 1;
+
+const idxBitsMask = [
+  0x80,
+  0x40,
+  0x20,
+  0x10,
+  0x8,
+  0x4,
+  0x2,
+  0x1,
+];
