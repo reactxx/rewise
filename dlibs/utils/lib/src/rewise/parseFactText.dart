3538d33c1c617998cd23fd61ca0abc8865de67dd
Pavel Zika <pzika@langmaster.cz>
2019-03-16 19:07:05 +0100

    fix

--- dlibs/utils/lib/src/rewise/parseFactText.dart
+++ dlibs/utils/lib/src/rewise/parseFactText.dart
@@ -1,17 +1,13 @@
 import 'package:rewise_low_utils/utils.dart' show Linq;
-import 'package:tuple/tuple.dart';
 
 class Parsed {
+  int idx;
+  int lessonId;
   String text;
   String breakText;
-
   String wcls;
-  String get stemmingText => _stemmingText ?? text;
-  String _stemmingText;
   List<Bracket> brackets;
   List<ParsedFact> child;
-  List<Tuple2<String, int>> errors;
-
 }
 
 Iterable<Parsed> parseFactTextFormat(String str) sync* {
@@ -21,15 +17,122 @@ Iterable<Parsed> parseFactTextFormat(String str) sync* {
 class Bracket {
   String type;
   String value;
-  static Bracket fromGroup(Match m, int idx) {
-    final val = m.group(idx);
-    if (val == null) return null;
-    return Bracket()
-      ..type = _types[idx]
-      ..value = m.input.substring(m.start + 1, m.end - 1);
+}
+
+class Error {
+  Error(this.id, this.code);
+  String id;
+  int code;
+}
+
+class ParsedFact {
+
+  String wcls;
+  String text;
+  String toBreakText;
+  List<Bracket> brackets;
+  List<ParsedFact> childs;
+  List<Error> errors;
+  String id;
+  String delim;
+  getToBreakText() => toBreakText ?? text;
+
+  ParsedFact(this.text,
+      [this.errors,
+      List<String> delims = const [Consts.wCls, Consts.wMean, Consts.wSyn],
+      this.id = '0']) {
+    if (_isRoot) errors = List<Error>();
+    // split fact by to tree by deimiters:|^,
+    for (var delim in delims) {
+      final d = text.split(delim);
+      if (d.length == 1) continue;
+      var childIdx = 0;
+      childs = d
+          .map((s) => ParsedFact(
+              s, errors, delims.skip(1).toList(), '$id.${childIdx++}'))
+          .toList();
+      this.delim = delim;
+      break;
+    }
+    // raw syntax check
+    if (_isLeaf && !_factTestRx.hasMatch(text)) {
+      brackets = [];
+      _addError(Consts.eSyntax);
+      return;
+    }
+    // leaf => parsing brackets
+    if (_isLeaf) {
+      brackets = List<Bracket>();
+      text = _processBrakets(false);
+      toBreakText = _processBrakets(true);
+      // 'empty without brakets' error
+      if (toBreakText.trim().isEmpty && text.isNotEmpty)
+        _addError(Consts.eEmptyWithoutBrackets);
+    }
+    // root:
+    if (_isRoot) {
+      //check and assign word class
+      final toCheck = delim == Consts.wCls ? childs : [this];
+      for (final toCh in toCheck) {
+        String cls;
+        for (final ch in toCh._childDeep()) {
+          final sqs = ch.brackets.where((b) => b.type == Consts.brSq).toList();
+          if (sqs.isEmpty) continue;
+          if (sqs.length > 1)
+            _addError(Consts.eWClsMore);
+          else if (!_firstChildChainRx
+              .hasMatch(ch.id.substring(toCh.id.length)))
+            _addError(Consts.eWClsOther);
+          else if (cls != null)
+            assert(false);
+          else
+            cls = sqs[0].value;
+        }
+        if (delim == Consts.wCls && cls == null)
+          _addError(Consts.eWClsMissing);
+        else if (cls != null) for (final ch in toCh._childDeep()) ch.wcls = cls;
+      }
+      // no error => join synonymous and remove non-leaf
+      if (errors.length == 0) {
+        errors = null;
+        brackets = Linq.selectMany(_childDeep(), (ParsedFact ch) => ch.brackets)
+            .toList();
+        for (final ch
+            in _childDeep(false).where((ch) => ch.delim == Consts.wSyn)) {
+          ch.text = ch.childs.map((cc) => cc.text).join(', ');
+          ch.wcls = ch.childs[0].wcls;
+          ch.toBreakText = ch.childs.map((cc) => cc.toBreakText).join(', ');
+          ch.childs = null;
+        }
+        childs = _childDeep().toList();
+        for (final ch in childs)
+          if (ch.text == ch.toBreakText) ch.toBreakText = null;
+      }
+    }
+  }
+  static final _firstChildChainRx = RegExp(r'^[0.]*$');
+  Iterable<ParsedFact> _childDeep([bool leafOnly = true]) sync* {
+    if (childs == null || !leafOnly) yield this;
+    if (childs != null) for (final ch in childs) yield* ch._childDeep(leafOnly);
   }
 
-  static const _types = <String>[null, Consts.br, Consts.brSq, Consts.brCur];
+  _processBrakets(bool forBreakText) {
+    return text.replaceAllMapped(_factBracketsRx, (m) {
+      final br = _bracketFromGroup(m, 1) ??
+          _bracketFromGroup(m, 2) ??
+          _bracketFromGroup(m, 3);
+      assert(br != null);
+      if (!forBreakText) brackets.add(br);
+      if (br.type == Consts.brSq) return '';
+      return forBreakText ? ''.padRight(m.end - m.start, ' ') : m.group(0);
+    });
+  }
+
+  _addError(int err) => errors.add(Error(id, err));
+
+  bool get _isLeaf => childs == null;
+  bool get _isRoot => id == '0';
+
 }
 
 class Consts {
@@ -46,119 +149,18 @@ class Consts {
   static const eEmptyWithoutBrackets = 4; // empty without brakets
 }
 
-class ParsedFact {
-  ParsedFact(this.text,
-      [this.errors,
-      List<String> delims = const [Consts.wCls, Consts.wMean, Consts.wSyn],
-      this.id = '0']) {
-    if (_isRoot) errors = List<Tuple2<String, int>>();
-    // split fact by to tree by deimiters:|^,
-    for (var delim in delims) {
-      final d = text.split(delim);
-      if (d.length == 1) continue;
-      var childIdx = 0;
-      child = d
-          .map((s) =>
-              ParsedFact(s, errors, delims.skip(1).toList(), '$id.${childIdx++}'))
-          .toList();
-      this.delim = delim;
-      break;
-    }
-    // raw syntax check
-    if (_isLeaf && !factTestRx.hasMatch(text)) {
-      brackets = [];
-      _addError(Consts.eSyntax);
-      return;
-    }
-    // leaf => parsing brackets
-    if (_isLeaf) {
-      brackets = List<Bracket>();
-      text = _processBrakets(false);
-      _stemmingText = _processBrakets(true);
-      if (_stemmingText == brackets) _stemmingText = null;
-      // 'empty without brakets' error
-      if (_stemmingText.trim().isEmpty && text.isNotEmpty)
-        _addError(Consts.eEmptyWithoutBrackets);
-    }
-    // root:
-    if (_isRoot) {
-      //check and assign word class
-      final toCheck = delim == Consts.wCls ? child : [this];
-      for (final toCh in toCheck) {
-        String cls;
-        for (final ch in toCh.childDeep()) {
-          final sqs = ch.brackets.where((b) => b.type == Consts.brSq).toList();
-          if (sqs.isEmpty) continue;
-          if (sqs.length > 1)
-            _addError(Consts.eWClsMore);
-          else if (!_firstChildChainRx
-              .hasMatch(ch.id.substring(toCh.id.length)))
-            _addError(Consts.eWClsOther);
-          else if (cls != null)
-            assert(false);
-          else
-            cls = sqs[0].value;
-        }
-        if (delim == Consts.wCls && cls == null)
-          _addError(Consts.eWClsMissing);
-        else if (cls != null) for (final ch in toCh.childDeep()) ch.wcls = cls;
-      }
-      // no error => join synonymous and remove non-leaf
-      if (errors.length == 0) {
-        errors = null;
-        brackets = Linq.selectMany(childDeep(), (ParsedFact ch) => ch.brackets).toList();
-        for (final ch
-            in childDeep(false).where((ch) => ch.delim == Consts.wSyn)) {
-          ch.text = ch.child.map((cc) => cc.text).join(', ');
-          ch.wcls = ch.child[0].wcls;
-          ch._stemmingText = ch.child.map((cc) => cc.toBreak).join(', ');
-          ch.child = null;
-          if (ch.text == ch._stemmingText) ch._stemmingText = null;
-          assert(ch.text.length == ch.toBreak.length);
-        }
-        child = childDeep().toList();
-      }
-    }
-  }
-  static final _firstChildChainRx = RegExp(r'^[0.]*$');
-  Iterable<ParsedFact> childDeep([bool leafOnly = true]) sync* {
-    if (child == null || !leafOnly) yield this;
-    if (child != null) for (final ch in child) yield* ch.childDeep(leafOnly);
+  Bracket _bracketFromGroup(Match m, int idx) {
+    final val = m.group(idx);
+    if (val == null) return null;
+    return Bracket()
+      ..type = _types[idx]
+      ..value = m.input.substring(m.start + 1, m.end - 1);
   }
 
-  _processBrakets(bool forBreakText) {
-    return text.replaceAllMapped(factBracketsRx, (m) {
-      final br = Bracket.fromGroup(m, 1) ??
-          Bracket.fromGroup(m, 2) ??
-          Bracket.fromGroup(m, 3);
-      assert(br != null);
-      if (!forBreakText) brackets.add(br);
-      if (br.type == Consts.brSq) return '';
-      return forBreakText ? ''.padRight(m.end - m.start, ' ') : m.group(0);
-    });
-  }
+  const _types = <String>[null, Consts.br, Consts.brSq, Consts.brCur];
 
-  _addError(int err) {
-    errors.add(Tuple2(id, err));
-  }
 
-  bool get _isLeaf => child == null;
-  bool get _isRoot => id == '0';
-
-  String wcls;
-  String delim;
-  String id;
-  String text;
-  String get toBreak => _stemmingText ?? text;
-  String _stemmingText;
-  List<Bracket> brackets;
-  List<ParsedFact> child;
-  List<Tuple2<String, int>> errors;
-}
-
-ParsedFact parseRaw(String str) => str.isEmpty ? null : ParsedFact(str);
-
-final factTestRx = RegExp(
+final _factTestRx = RegExp(
     r'^(\([^(){}\[\],|^]+\)|\[\w+\]|{[^(){}\[\],|^]+}|[^(){}\[\],|^]*)+$');
-final factBracketsRx =
+final _factBracketsRx =
     RegExp(r'(\([^(){}\[\],|^]+\))|(\[\w+\])|({[^(){}\[\],|^]+})');
--- dlibs/utils/test/rewise/parser_test.dart
+++ dlibs/utils/test/rewise/parser_test.dart
@@ -28,27 +28,27 @@ main() {
       // value checks
       // Errors checks
       it = rw.ParsedFact('(d   ');
-      test.expect(it.errors[0].item2, test.equals(rw.Consts.eSyntax));
+      test.expect(it.errors[0].code, test.equals(rw.Consts.eSyntax));
       it = rw.ParsedFact('d)');
-      test.expect(it.errors[0].item2, test.equals(rw.Consts.eSyntax));
+      test.expect(it.errors[0].code, test.equals(rw.Consts.eSyntax));
       it = rw.ParsedFact('d[');
-      test.expect(it.errors[0].item2, test.equals(rw.Consts.eSyntax));
+      test.expect(it.errors[0].code, test.equals(rw.Consts.eSyntax));
       it = rw.ParsedFact('d]');
-      test.expect(it.errors[0].item2, test.equals(rw.Consts.eSyntax));
+      test.expect(it.errors[0].code, test.equals(rw.Consts.eSyntax));
       it = rw.ParsedFact('d{');
-      test.expect(it.errors[0].item2, test.equals(rw.Consts.eSyntax));
+      test.expect(it.errors[0].code, test.equals(rw.Consts.eSyntax));
       it = rw.ParsedFact('}d');
-      test.expect(it.errors[0].item2, test.equals(rw.Consts.eSyntax));
+      test.expect(it.errors[0].code, test.equals(rw.Consts.eSyntax));
 
       it = rw.ParsedFact(' [w]  {s} (z)   ');
-      test.expect(it.errors[0].item2, test.equals(rw.Consts.eEmptyWithoutBrackets));
+      test.expect(it.errors[0].code, test.equals(rw.Consts.eEmptyWithoutBrackets));
 
       it = rw.ParsedFact('d^[w] d');
-      test.expect(it.errors[0].item2, test.equals(rw.Consts.eWClsOther));
+      test.expect(it.errors[0].code, test.equals(rw.Consts.eWClsOther));
       it = rw.ParsedFact('d^[w] d [w]');
-      test.expect(it.errors[0].item2, test.equals(rw.Consts.eWClsMore));
+      test.expect(it.errors[0].code, test.equals(rw.Consts.eWClsMore));
       it = rw.ParsedFact('d|d[w]');
-      test.expect(it.errors[0].item2, test.equals(rw.Consts.eWClsMissing));
+      test.expect(it.errors[0].code, test.equals(rw.Consts.eWClsMissing));
       it = rw.ParsedFact('d(|d[w]');
       test.expect(it.errors.length, test.equals(2));
     });
@@ -56,12 +56,12 @@ main() {
     test.test('join synonymous', () {
       rw.ParsedFact it;
       it = rw.ParsedFact('[w]d|[w]d(b),e{b},f');
-      test.expect(it.child[1].text, test.equals('d(b), e{b}, f'));
-      test.expect(it.child[1].toBreak, test.equals('d   , e   , f'));
+      test.expect(it.childs[1].text, test.equals('d(b), e{b}, f'));
+      test.expect(it.childs[1].getToBreakText(), test.equals('d   , e   , f'));
       it = rw.ParsedFact('[w]d^{1}d,(2)d|[w]d(3),e{4},f^d(5),e{6},f');
       final brs = it.brackets.map((b)=>b.value).join(';');
-      final cht = it.child.map((b)=>b.text).join(';');
-      final chb = it.child.map((b)=>b.toBreak).join(';');
+      final cht = it.childs.map((b)=>b.text).join(';');
+      final chb = it.childs.map((b)=>b.getToBreakText()).join(';');
       test.expect(brs, test.equals('w;1;2;w;3;4;5;6'));
       test.expect(cht, test.equals('d;{1}d, (2)d;d(3), e{4}, f;d(5), e{6}, f'));
       test.expect(chb, test.equals('d;   d,    d;d   , e   , f;d   , e   , f'));
