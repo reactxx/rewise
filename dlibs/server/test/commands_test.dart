4cc3933e3d31607ce0ac68f38c22bbf332506227
Pavel Zika <pzika@langmaster.cz>
2019-03-20 13:28:09 +0100

    fix

--- dlibs/server/test/commands_test.dart
+++ dlibs/server/test/commands_test.dart
@@ -7,18 +7,31 @@ import 'package:test/test.dart';
 import 'package:server_dart/commands.dart';
 import 'package:rw_low/code.dart' show Linq;
 
-  main() {
+main() {
   group("commands", () {
-
     test('toRaw', () async {
       var resp = await toRaw();
       expectLater(resp.isEmpty, equals(true), reason: resp);
     }, skip: true);
 
     test('toParsed', () async {
-      var res = await Future.wait(Linq.range(0,100).map((idx) => toParsed()));
-      expectLater(res!=null, equals(true), reason: '');
+      print(DateTime.now().toString());
+      var res = await toParsed();
+      print(DateTime.now().toString());
+      expectLater(res != null, equals(true), reason: '');
+    }, skip: true);
+
+    test('toParsed', () async {
+      print('*' + DateTime.now().toString());
+      await Future.wait(Linq.range(0, 100).map((idx) => run(idx)));
+      print('*' + DateTime.now().toString());
     }, skip: false);
-    
   }, skip: true);
+}
+
+Future run(int idx) async {
+  await Future.delayed(Duration(seconds: idx * 3));
+  await toParsed();
+  print('- ' + DateTime.now().toString());
+  Future.value(null);
 }
