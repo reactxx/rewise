76b17a9d2d85076240d2da72429c30ac233f1329
Pavel Zika <pzika@langmaster.cz>
2019-03-27 10:24:47 +0100

    fix

--- dlibs/utils/lib/src/rewise/parsing/breaking.dart
+++ dlibs/utils/lib/src/rewise/parsing/breaking.dart
@@ -3,16 +3,21 @@ import 'package:rw_low/code.dart' show Linq;
 import 'package:rw_utils/client.dart' as client;
 import 'package:rw_utils/dom/word_breaking.dart' as wbreak;
 
-Future<rewise.ParseBookResult> wordBreaking (rewise.ParseBookResult parsed) async { 
-    // word breaking
-    final futures = parsed.book.books.map((book) => client.WordBreaking_Run(
-        wbreak.Request()
-          ..lang = book.lang
-          ..facts.addAll(
-              rewise.forBreaking(book).map((ch) => ch.breakText.isNotEmpty ? ch.breakText : ch.text))));
-    final booksBreaks = await Future.wait(futures);
-    for (var pair in Linq.zip(parsed.book.books, booksBreaks))
-      rewise.megreBreaking(pair.item1, pair.item2, parsed.errors);
-    return Future.value(parsed);
+Future<rewise.ParseBookResult> wordBreaking(
+    rewise.ParseBookResult parsed) async {
+  // word breaking
+  final futures = parsed.book.books.map((book) {
+    final facts = rewise
+          .forBreaking(book)
+          .map((ch) => ch.breakText.isNotEmpty ? ch.breakText : ch.text).toList();
+    print(facts.length);
+    final req = wbreak.Request()
+      ..lang = book.lang
+      ..facts.addAll(facts);
+    return client.WordBreaking_Run(req);
+  });
+  final booksBreaks = await Future.wait(futures);
+  for (var pair in Linq.zip(parsed.book.books, booksBreaks))
+    rewise.megreBreaking(pair.item1, pair.item2, parsed.errors);
+  return Future.value(parsed);
 }
-
