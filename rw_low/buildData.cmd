db42e52a9f796e460ff3a2d877a783f33ca63562
Pavel Zika <pzika@langmaster.cz>
2019-04-03 10:29:59 +0200

    fix

--- rw_low/buildData.cmd
+++ rw_low/buildData.cmd
@@ -13,14 +13,21 @@ cd %root%dlibs\utils
 rem rmdir %root%data\02_raw /s /q
 rem call dart lib\src\rewise\toRawMain.dart
 
-rem rmdir %root%data\03_parsed /s /q
-rem call dart lib\src\rewise\parsing\toParsedMain.dart
+rmdir %root%data\03_parsed /s /q
+call dart lib\src\rewise\parsing\toParsedMain.dart
 
-rem rmdir %root%data\log\parsed /s /q
-rem call dart lib\src\rewise\stat\statMain.dart
+rmdir %root%data\log\parsed /s /q
+call dart lib\src\rewise\stat\statMain.dart
+
+rem rmdirx %root%data\stemmCache /s /q
+rem call dart lib\src\rewise\stemming\stemmingMain.dart
+
+rem ---- STATs
+
+rmdir %root%data\log\parsed /s /q
+call dart lib\src\rewise\stat\statMain.dart
 
 rem rmdir %root%data\log\stemmed /s /q
 rem call dart lib\src\rewise\stat\statStemmMain.dart
 
-rem rmdirx %root%data\stemmCache /s /q
-rem call dart lib\src\rewise\stemming\stemmingMain.dart
+
