0cbe6280a21ee5fbedb0b6d53c448cc1f157bc37
Pavel Zika <pzika@langmaster.cz>
2019-04-17 22:55:03 +0200

    fix

--- dlibs/utils/lib/src/rewise/sources/batches/analyzeWords.dart
+++ dlibs/utils/lib/src/rewise/sources/batches/analyzeWords.dart
@@ -7,26 +7,31 @@ import '../dom.dart';
 
 void analyzeWords(FileInfo first, List<Tuple2<FileInfo, Word>> fileWords,
     String pathFragment) {
-  final map = HashMap<AnalyzeResult, int>();
-  final wmap = HashMap<String, int>();
+  final map = HashMap<AnalyzeResult, W>();
   for (final fw in fileWords.where((w) =>
-      w.item2.text.isNotEmpty &&
+      w.item2.text.isNotEmpty && 
       (w.item2.flags & Flags.wInBr == 0) &&
       (w.item2.flags & Flags.wHasParts == 0))) {
     final res = analyzeWord(fw.item1, fw.item2);
-    map.update(res, (v) => v + 1, ifAbsent: () => 1);
-    wmap.update(fw.item2.text, (v) => v + 1, ifAbsent: () => 1);
+    map.update(res, (v) {
+      v.count++;
+      if (v.words.length < 100) v.words.add(fw.item2.text);
+      return v;
+    }, ifAbsent: () => W(fw.item2.text));
   }
 
-  final list = List<MapEntry<AnalyzeResult, int>>.from(map.entries);
-  list.sort((a, b) => b.value - a.value);
-  final lines = list.map((kv) => '${kv.value}x: ${kv.key.toString()}');
+  final list = List<MapEntry<AnalyzeResult, W>>.from(map.entries);
+  list.sort((a, b) => b.value.count - a.value.count);
+  final lines = list.map((kv) => '${kv.value.count}x: |${kv.key.toString()}| ${kv.value.words.join('|')} ');
   fileSystem.edits.writeAsLines('analyzeSources\\$pathFragment.txt', lines);
+}
 
-  final wlist = List<MapEntry<String, int>>.from(wmap.entries);
-  wlist.sort((a, b) => b.value - a.value);
-  final wlines = list.map((kv) => '${kv.value}x: ${kv.key}');
-  fileSystem.edits.writeAsLines('analyzeSources\\${pathFragment}List.txt', wlines);
+class W {
+  W(String w) {
+    words.add(w);
+  }
+  int count = 1;
+  final words = List<String>();
 }
 
 AnalyzeResult analyzeWord(FileInfo file, Word word) {
