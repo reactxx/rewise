761bedd31f37090dcd3d04f0973ae7481f6d9be1
Pavel Zika <pzika@langmaster.cz>
2019-05-02 21:18:45 +0200

    fix

--- dlibs/utils/lib/src/rewise/trans/trans.dart
+++ dlibs/utils/lib/src/rewise/trans/trans.dart
@@ -1,14 +1,17 @@
 import 'dart:convert' as conv;
 import 'package:rw_utils/utils.dart' as ut;
+import 'package:path/path.dart' as p;
 import 'package:rw_utils/threading.dart';
 import '../sources/filer.dart';
 import '../sources/consts.dart';
 import 'package:rw_utils/utils.dart' show fileSystem;
 
-
-Future toTrans({bool doParallel, bool emptyPrint}) async =>
-    useSources(_toTransEntryPoint, _toTrans, GroupByType.dataLang,
-        emptyPrint: emptyPrint, doParallel: doParallel);
+Future toTrans({bool doParallel, bool emptyPrint}) async {
+  // await useSources(_toTransEntryPoint, _toTrans, GroupByType.dataLang,
+  //     emptyPrint: emptyPrint, doParallel: doParallel);
+  var files = fileSystem.transTasks.list(regExp: r'\.html$').map((f) => p.basenameWithoutExtension(f)).toList();
+  fileSystem.transTasks.writeAsString('content.json', conv.jsonEncode(files));
+}
 
 void _toTransEntryPoint(List workerInitMsg) =>
     parallelEntryPoint(workerInitMsg, _toTrans);
@@ -19,24 +22,25 @@ Future<Msg> _toTrans(DataMsg msg, InitMsg initPar) async {
 
   var wset = uniqueFilesWords(infos);
   var count = 0;
-  for(var intv in ut.Interval.intervalsMaxLen(wset.length, 10000)) {
+  for (var intv in ut.Interval.intervalsMaxLen(wset.length, 10000)) {
     var sb = StringBuffer();
     sb.write(_htmlStart(first.dataLang));
-    for(var w in wset.skip(intv.skip).take(intv.take)) sb.write(_htmlWord(w));
+    for (var w in wset.skip(intv.skip).take(intv.take)) sb.write(_htmlWord(w));
     sb.write(_htmlEnd);
-    fileSystem.transTasks.writeAsString('${first.dataLang}.${count.toString()}.html', sb.toString());
+    fileSystem.transTasks.writeAsString(
+        '${first.dataLang}.${count.toString()}.html', sb.toString());
     count++;
   }
 
   return Parallel.workerReturnFuture;
 }
 
-String _htmlStart (String lang) => 
-'''<!DOCTYPE html>
+String _htmlStart(String lang) =>
+    '''<!DOCTYPE html>
 
-<html lang="'''
-+ lang +
-'''">
+<html lang="''' +
+    lang +
+    '''">
 
 <head>
   <meta charset="UTF-8">
@@ -51,7 +55,7 @@ String _htmlStart (String lang) =>
   <h1 id="title"></h1>
   <div>
 ''';
-String _htmlWord (String w) => '<p>${conv.htmlEscape.convert(w)}</p>';
+String _htmlWord(String w) => '<p>${conv.htmlEscape.convert(w)}</p>';
 
 String _htmlEnd = '''
   </div>
