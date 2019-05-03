81678d95f726b5e01c65140fd4a1f505083b7309
Pavel Zika <pzika@langmaster.cz>
2019-05-03 09:15:26 +0200

    fix

--- dlibs/utils/lib/src/rewise/trans/trans.dart
+++ dlibs/utils/lib/src/rewise/trans/trans.dart
@@ -10,12 +10,16 @@ import 'package:rw_utils/langs.dart' show Langs;
 Future toTrans({bool doParallel, bool emptyPrint}) async {
   await useSources(_toTransEntryPoint, _toTrans, GroupByType.dataLang,
       emptyPrint: emptyPrint, doParallel: doParallel, filter: _filter);
-  var files = fileSystem.transTasks.list(regExp: r'\.html$').map((f) => p.basenameWithoutExtension(f)).toList();
+  var files = fileSystem.transTasks
+      .list(regExp: r'\.html$')
+      .map((f) => p.basenameWithoutExtension(f))
+      .toList();
   fileSystem.transTasks.writeAsString('_content.json', conv.jsonEncode(files));
+  fileSystem.transTasks.writeAsString('_content.html', _contentHtml(files));
 }
 
 bool _filter(FileInfo fi) {
-  if (fi.dataLang=='en-GB' || fi.dataLang=='en-US') return false;
+  if (fi.dataLang == 'en-GB' || fi.dataLang == 'en-US') return false;
   return Langs.nameToMeta[fi.dataLang].googleTransId.isNotEmpty;
 }
 
@@ -26,11 +30,12 @@ Future<Msg> _toTrans(DataMsg msg, InitMsg initPar) async {
   Iterable<FileInfo> infos = scanFileInfos(msg);
   FileInfo first = infos.first;
 
+  var googleLang = Langs.nameToMeta[first.dataLang].googleTransId;
   var wset = uniqueFilesWords(infos);
   var count = 0;
-  for(var intv in ut.Interval.intervalsMaxLen(wset.length, 5000)) {
+  for (var intv in ut.Interval.intervalsMaxLen(wset.length, 5000)) {
     var sb = StringBuffer();
-    sb.write(_htmlStart(first.dataLang));
+    sb.write(_htmlStart(googleLang));
     for (var w in wset.skip(intv.skip).take(intv.take)) sb.write(_htmlWord(w));
     sb.write(_htmlEnd);
     fileSystem.transTasks.writeAsString(
@@ -43,11 +48,9 @@ Future<Msg> _toTrans(DataMsg msg, InitMsg initPar) async {
 
 String _htmlStart(String lang) =>
     '''<!DOCTYPE html>
-
 <html lang="''' +
     lang +
     '''">
-
 <head>
   <meta charset="UTF-8">
   <title>Web App</title>
@@ -56,15 +59,33 @@ String _htmlStart(String lang) =>
     p {display: inline-block; width: 100px; margin: 0; white-space: nowrap; }
   </style>
 </head>
-
 <body>
   <div>
 ''';
 String _htmlWord(String w) => '<p>${conv.htmlEscape.convert(w)}</p>';
-
 String _htmlEnd = '''
   </div>
 </body>
+</html>
+''';
 
+String _contentHtml(List<String> urls) {
+  var sb = StringBuffer();
+  sb.write(_contentStart);
+  for (var url in urls) sb.write(_contentItem(url));
+  sb.write(_contentEnd);
+  return sb.toString();
+}
+String _contentStart = '''<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Web App</title>
+</head>
+<body>
+''';
+String _contentItem(String url) => '<p><a href="$url.html">$url</a></p>';
+String _contentEnd = '''
+</body>
 </html>
 ''';
