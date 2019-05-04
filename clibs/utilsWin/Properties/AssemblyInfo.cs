e7dbd98ddf1316131bbe8f26802b86b1fe1493b2
Pavel Zika <pzika@langmaster.cz>
2019-05-04 11:37:54 +0200

    refactor

--- clibs/utilsWin/Properties/AssemblyInfo.cs
+++ clibs/utilsWin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LangsLib")]
+[assembly: AssemblyTitle("utilsWin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LangsLib")]
+[assembly: AssemblyProduct("utilsWin")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f602e5e0-bc5c-478e-86b0-1103411cb03a")]
+[assembly: Guid("c93470d3-1610-47d4-924f-de0c4ae56018")]
 
 // Version information for an assembly consists of the following four values:
 //
