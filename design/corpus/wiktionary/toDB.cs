4529e7733bc76321b7057966d20da8e58d9cdf1b
Pavel Zika <pzika@langmaster.cz>
2019-05-18 14:15:06 +0200

    fix

--- design/corpus/wiktionary/toDB.cs
+++ design/corpus/wiktionary/toDB.cs
@@ -1,31 +1,114 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using VDS.RDF;
 
-namespace WiktModel {
-  public class Page {
-    public int Id { get; set; }
-    public string Title { get; set; }
+public static class WiktToDB {
+
+  public static void replaceUrlByIds() {
+    foreach (var lang in WiktQueries.allLangs) replaceUrlById(lang);
   }
-  public class Entry {
-    public int Id { get; set; }
-    public int PageId { get; set; }
-    public string PartOfSpeech { get; set; }
-    public string Title { get; set; } // title;note
-    public string[] Other { get; set; } // [title;note]
+
+  static Dictionary<string, Dictionary<string, int>> idsToMemory(string dir) {
+    var res = new Dictionary<string, Dictionary<string, int>>();
+    var idFiles = Directory.GetFiles(dir, "*.ttl").Where(fn => Path.GetFileNameWithoutExtension(fn).ToLower().StartsWith("ids_")).ToArray();
+    Parallel.ForEach(idFiles, new ParallelOptions { MaxDegreeOfParallelism = 4 }, fn => {
+      var clsName = Path.GetFileNameWithoutExtension(fn).ToLower().Split('_')[1];
+      var destFn = Path.ChangeExtension(fn, ".txt");
+      Dictionary<string, int> ids;
+      //if (File.Exists(destFn)) {
+      //  var count = 0;
+      //  ids = File.ReadAllLines(destFn).ToDictionary(id => id, id => count++);
+      //} else
+      using (var wr = new StreamWriter(destFn)) {
+        ids = new Dictionary<string, int>();
+        VDS.LM.Parser.parse(fn, (trip, count) => {
+          var txt = trip.Object as LiteralNode;
+          wr.WriteLine(txt.Value);
+          ids[txt.Value] = ids.Count + 1;
+        });
+      }
+      lock (res) res[clsName] = ids;
+    });
+    return res;
   }
-  public class Trans {
-    public int Id { get; set; }
-    public int EntryId { get; set; }
-    public int SenseId { get; set; }
-    public ushort Lang { get; set; }
-    // gloss
-    public string GlossRank { get; set; }
-    public string Gloss { get; set; }
+
+  public static void replaceUrlById(string lang) {
+    var dir = Corpus.Dirs.wikiesDbnary + @"graphDBExport\" + lang;
+    if (!Directory.Exists(dir)) return;
+    var files = Directory.GetFiles(dir).Select(fn => {
+      var parts = Path.GetFileNameWithoutExtension(fn).ToLower().Split('_');
+      var isProp = parts[0] == "prop" ? true : (parts[0] == "rel" ? false : (bool?)null);
+      if (isProp == null) return null;
+      return new ttlFile { fn = fn, name = Path.GetFileNameWithoutExtension(fn), isProp = (bool)isProp, fromCls = parts[1], predicate = parts[2], toCls = isProp == true ? null : parts[3] };
+    }).
+    Where(t => t != null).
+    ToArray();
+
+    var ids = idsToMemory(dir);
+
+    using (var wrProp = new StreamWriter(dir + @"\props.txt"))
+    using (var wrRel = new StreamWriter(dir + @"\rels.txt"))
+      foreach (var file in files) {
+        var wr = file.isProp ? wrProp : wrRel;
+        VDS.LM.Parser.parse(file.fn, (trip, count) => {
+          wr.Write(WiktQueries.nameToId[file.fromCls]); wr.Write('|');
+          wr.Write(ids[file.fromCls][(trip.Subject as UriNode).Uri.LocalPath]); wr.Write('|');
+          wr.Write(propToId[file.name]); wr.Write('|');
+          if (file.isProp) {
+            var obj = trip.Object;
+            if (obj is LiteralNode) wr.Write((obj as LiteralNode).Value);
+            else if (obj is UriNode) wr.Write((obj as UriNode).Uri.LocalPath);
+            else throw new Exception();
+          } else {
+            wr.Write(WiktQueries.nameToId[file.toCls]); wr.Write('|');
+            var objId = (trip.Object as UriNode).Uri.LocalPath;
+            try {
+              wr.Write(ids[file.toCls][objId]);
+            } catch {
+              wr.Write("???" + objId);
+            }
+          }
+          wr.WriteLine();
+        });
+      }
   }
-  public class Sense {
-    public int Id { get; set; }
+
+  static Dictionary<string, string> propToId = new Dictionary<string, string> {
+    {"prop_entry_language","el"},
+    {"prop_entry_partofspeech","ep"},
+    {"prop_form_note","fn"},
+    {"prop_form_phoneticrep","fp"},
+    {"prop_form_writtenrep","fw"},
+    {"prop_gloss_rank","gr"},
+    {"prop_gloss_sensenumber","gs"},
+    {"prop_gloss_value","gv"},
+    {"prop_sense_number","sn"},
+    {"prop_trans_targetlanguagecode","tt"},
+    {"prop_trans_usage","tu"},
+    {"prop_trans_writtenform","tw"},
+    {"rel_entry_canform_form","ecf"},
+    {"rel_entry_otherform_form","eof"},
+    {"rel_entry_sense_sense","ess"},
+    {"rel_entry_synonyms_page","esp"},
+    {"rel_page_descr_entry","pde"},
+    {"rel_page_descr_multi","pdm"},
+    {"rel_page_synonyms_page","psp"},
+    {"rel_sense_synonyms_page","ssp"},
+    {"rel_trans_gloss_gloss","tgg"},
+    {"rel_trans_trans_entry","tte"},
+    {"rel_trans_trans_sense","tts"},
+  };
+
+
+  class ttlFile {
+    public string fn;
+    public string name;
+    public string fromCls;
+    public bool isProp;
+    public string predicate;
+    public string toCls; // for isProp=false
   }
 }
