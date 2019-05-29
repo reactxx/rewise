e59aede07b07c06ef9bb478a1dd06c1adb36c02b
Pavel Zika <pzika@langmaster.cz>
2019-05-29 14:29:18 +0200

    fix

--- design/corpus/wiktionary/logger.cs
+++ design/corpus/wiktionary/logger.cs
@@ -6,40 +6,35 @@ public class WiktLogger {
 
   public void add(string lang, string cls, string prop, string enumVal) {
     lock (this) {
-      addToList(L0123, new[] { lang, cls, prop, enumVal });
-      addToList(L012, new[] { lang, cls, prop });
-      addToList(L123, new[] { cls, prop, enumVal });
-      addToList(L12, new[] { cls, prop });
+      addToList(D123, new[] { lang, cls, prop, enumVal });
+      addToList(D12, new[] { lang, cls, prop });
     }
   }
 
-  void addToList(List<string> list, string[] vals) {
-    var s = "";
-    list.Add(s);
+  void addToList(Dictionary<string, int> list, string[] vals) {
+    void add(string str) => list[str] = list.TryGetValue(str, out int c) ? c + 1 : 1;
+
+    var s = ""; var t = "";
+    add(s);
     for (var i = 0; i < vals.Length; i++) {
       if (vals[i] == null) break;
-      if (i > 0) s += "=";
-      s += vals[i];
-      list.Add(s);
+      if (i > 0) { s += "="; t += "="; }
+      s += vals[i]; t += i == 0 ? "**" : vals[i];
+      add(s); add(t);
     }
   }
 
   public void save(string fn) {
-    if (L0123.Count == 0) return;
-    save(fn + ".0123.txt", L0123);
-    save(fn + ".012.txt", L012);
-    save(fn + ".123.txt", L123);
-    save(fn + ".12.txt", L12);
+    if (D123.Count() == 0) return;
+    save(fn + ".123.txt", D123);
+    save(fn + ".12.txt", D12);
   }
 
-  void save(string fn, List<string> list) => 
-    File.WriteAllLines(fn, list.GroupBy(s => s).OrderBy(g => g.Key).Select(g => $"{g.Key} {g.Count()}"));
+  void save(string fn, Dictionary<string, int> list) =>
+    File.WriteAllLines(fn, list.OrderBy(g => g.Key).Select(g => $"{g.Key} {g.Value}"));
 
-  List<string> L0123 = new List<string>();
-  List<string> L012 = new List<string>();
-  List<string> L123 = new List<string>();
-  List<string> L12 = new List<string>();
-
+  Dictionary<string, int> D123 = new Dictionary<string, int>();
+  Dictionary<string, int> D12 = new Dictionary<string, int>();
 }
 
 
