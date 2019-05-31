c46e73e7d30b157ede95e83b793f38193bcd39a1
Pavel Zika <pzika@langmaster.cz>
2019-05-31 13:17:07 +0200

    fix

--- design/corpus/wiki/rawParser.cs
+++ design/corpus/wiki/rawParser.cs
@@ -6,12 +6,18 @@ using WikimediaProcessing;
 
 public static class WikiRawParser {
 
+  public class WikiItem {
+    public string lang;
+    public string[] types;
+    public int[] articles;
+  }
+
   public static string[] types;
   public static string[] langs;
 
   public static void run(string lang) {
 
-    var allWikiLangs = Wiki.WikiStat.load().Where(l => l.lang != null).Select(l => l.lang).ToHashSet();
+    var allWikiLangs = Wiki.WikiStat.load().Where(l => l.lang != null).ToDictionary(l => l.lang);
 
     var tls = Directory.EnumerateFiles(Corpus.Dirs.wikiesRaw).
       Where(f => Path.GetExtension(f) == "").
@@ -20,10 +26,16 @@ public static class WikiRawParser {
         if (parts.Length != 2) return null;
         return new { lang = parts[0], type = "wi" + parts[1] };
       }).
-      Where(lt => lt != null && allWikiLangs.Contains(lt.lang)).
+      Select(lt => new { item = allWikiLangs.TryGetValue(lt.lang, out Wiki.WikiStat it) ? it : null, lt.type }).
+      Where(lt => lt.item != null).
       ToArray();
     var types = string.Join(",", tls.Select(tl => tl.type).Distinct().OrderBy(s => s).Select(s => $"\"{s}\""));
-    var langs = string.Join(",", tls.Select(tl => tl.lang).Distinct().OrderBy(s => s).Select(s => $"\"{s}\""));
+    var items = tls.GroupBy(tl => tl.item.lang).Select(g => new WikiItem {
+      lang = g.First().item.lang,
+      types = g.Select(it => it.type).ToArray(),
+      articles = g.Select(it => it.item.articles).ToArray(),
+    }).ToArray();
+    Json.Serialize(@"C:\rewise\design\corpus\wiki\rawParser.json", items);
 
     void dumpSection(WikiSection sect) {
     }
